a903d3475e247452ca4262ef85ed6765c986e0bd
Anush P <av.pogosyan@gmail.com>
2022-05-04 19:48:21 -0700

    added empty file for migrations + update env variables

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export SQLALCHEMY_DATABASE_URI="postgresql://zgnixvdsixthhn:418a90d2c780e2595f502b62860566d6db3177ecfd9eb9397062196cdb60a3af@ec2-18-210-64-223.compute-1.amazonaws.com:5432/d70aam0q9vam5q"
+export DATABASE_URL="postgresql://zgnixvdsixthhn:418a90d2c780e2595f502b62860566d6db3177ecfd9eb9397062196cdb60a3af@ec2-18-210-64-223.compute-1.amazonaws.com:5432/d70aam0q9vam5q"
 export SQLALCHEMY_TEST_DATABASE_URI="postgresql://zgnixvdsixthhn:418a90d2c780e2595f502b62860566d6db3177ecfd9eb9397062196cdb60a3af@ec2-18-210-64-223.compute-1.amazonaws.com:5432/d70aam0q9vam5q"
 export SQLALCHEMY_TRACK_MODIFICATIONS="false"
 export AUTH0_DOMAIN='dev-lyhnb9r1.us.auth0.com'
