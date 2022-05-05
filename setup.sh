ebb51bdfa35c9bf36beca702a6e0223050114510
Anush P <av.pogosyan@gmail.com>
2022-05-04 19:43:39 -0700

    update env vars

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export SQLALCHEMY_DATABASE_URI="postgresql://hetznfjkmvarqt:fdc44632cd98161ac6b08b887b1a86efc82cf3bfc275c2ff1579f5d8c1f5ea06@ec2-54-172-175-251.compute-1.amazonaws.com:5432/d3djidrgqg7qot"
-export SQLALCHEMY_TEST_DATABASE_URI="postgresql://hetznfjkmvarqt:fdc44632cd98161ac6b08b887b1a86efc82cf3bfc275c2ff1579f5d8c1f5ea06@ec2-54-172-175-251.compute-1.amazonaws.com:5432/d3djidrgqg7qot"
+export SQLALCHEMY_DATABASE_URI="postgresql://zgnixvdsixthhn:418a90d2c780e2595f502b62860566d6db3177ecfd9eb9397062196cdb60a3af@ec2-18-210-64-223.compute-1.amazonaws.com:5432/d70aam0q9vam5q"
+export SQLALCHEMY_TEST_DATABASE_URI="postgresql://zgnixvdsixthhn:418a90d2c780e2595f502b62860566d6db3177ecfd9eb9397062196cdb60a3af@ec2-18-210-64-223.compute-1.amazonaws.com:5432/d70aam0q9vam5q"
 export SQLALCHEMY_TRACK_MODIFICATIONS="false"
 export AUTH0_DOMAIN='dev-lyhnb9r1.us.auth0.com'
 export ALGORITHM='RS256'
